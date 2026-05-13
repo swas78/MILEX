a36a8322d273a6e9a33c6a5983fa7064a440bb96
swas78 <swastikmishra2025@gmail.com>
2026-05-13 22:08:03 +0530

    Fix: Remove output:export to resolve Vercel NOT_FOUND error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-if (typeof global !== 'undefined' && typeof global.localStorage !== 'undefined' && !global.localStorage.getItem) {
-  global.localStorage.getItem = () => null;
-  global.localStorage.setItem = () => {};
-  global.localStorage.removeItem = () => {};
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
